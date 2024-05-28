5561c1eb5cadb68f3031f678d310d33abd805255
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-05-28 16:50:49 +0100

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,8 @@ from ..extensions import db
 from config import Config
 from enum import Enum
 
-from app.models import Media, Stock, Business, RealEstate, Crypto, NFT
+from app.models import Media
+# , Stock, Business, RealEstate, Crypto, NFT
 from app.models.role import Role, RoleNames
 
 class TempUser(db.Model):
