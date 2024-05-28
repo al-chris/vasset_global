d9d05857cf1e8b303e2ef2506d9fc285bc6d2990
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-05-28 22:13:18 +0100

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -39,7 +39,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    _password = db.Column(db.String(128))
+    _password = db.Column(db.String(256))
     email_verified = db.Column(db.Boolean, default=False)
     phone_verified = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
