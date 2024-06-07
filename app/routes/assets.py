f06aeea305050ac4e60b02edf7fbd3f2f7400b44
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-06-07 13:26:30 +0100

    migrations + return statements

--- app/routes/assets.py
+++ app/routes/assets.py
@@ -24,72 +24,72 @@ from app.views import AssetsController
 @api.route('/users/stocks', methods=['POST'])
 @jwt_required()
 def add_stock():
-    AssetsController.add_stock()
+    return AssetsController.add_stock()
 
 @api.route('/users/stocks', methods=['GET'])
 @jwt_required()
 def get_stocks():
-    AssetsController.get_stocks()
+    return AssetsController.get_stocks()
 
 # RealEstate endpoints
 @api.route('/users/real_estates', methods=['POST'])
 @jwt_required()
 def add_real_estate():
-    AssetsController.add_real_estate()
+    return AssetsController.add_real_estate()
 
 @api.route('/users/real_estates', methods=['GET'])
 @jwt_required()
 def get_real_estates():
-    AssetsController.get_real_estates()
+    return AssetsController.get_real_estates()
 
 # Business endpoints
 @api.route('/users/businesses', methods=['POST'])
 @jwt_required()
 def add_business():
-    AssetsController.add_business()
+    return AssetsController.add_business()
 
 @api.route('/users/businesses', methods=['GET'])
 @jwt_required()
 def get_businesses():
-    AssetsController.get_businesses()
+    return AssetsController.get_businesses()
 
 # Crypto endpoints
 @api.route('/users/cryptos', methods=['POST'])
 @jwt_required()
 def add_crypto():
-    AssetsController.add_crypto()
+    return AssetsController.add_crypto()
 
 @api.route('/users/cryptos', methods=['GET'])
 @jwt_required()
 def get_cryptos():
-    AssetsController.get_cryptos()
+    return AssetsController.get_cryptos()
 
 # NFT endpoints
 @api.route('/users/nfts', methods=['POST'])
 @jwt_required()
 def add_nft():
-    AssetsController.add_nft()
+    return AssetsController.add_nft()
 
 @api.route('/users/nfts', methods=['GET'])
 @jwt_required()
 def get_nfts():
-    AssetsController.get_nfts()
+    return AssetsController.get_nfts()
 
 # SocialMedia endpoints
 @api.route('/users/social_media', methods=['POST'])
 @jwt_required()
 def add_social_media():
-    AssetsController.add_social_media()
+    return AssetsController.add_social_media()
 
 
 @api.route('/users/social_media', methods=['GET'])
 @jwt_required()
 def get_social_media():
-    AssetsController.get_social_media()
+    return AssetsController.get_social_media()
 
 # Get all assets
 @api.route('/users/assets', methods=['GET'])
 @jwt_required()
 def get_all_assets():
-    AssetsController.get_all_assets()
+    return AssetsController.get_all_assets()
 # app/views/assets.py
